034429c046618adb29d1df2c7b378aaca94b2ab1
FaisalAlwazir <eBNaLWAZIR95>
2020-09-16 18:55:43 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/c_assignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_assignment.dir/main.c.obj"
+  "C_assignment.exe"
+  "C_assignment.exe.manifest"
+  "C_assignment.pdb"
+  "libC_assignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
